ffc1e878c84b4bec0b0bfa5570be99c2af4b4c53
Paavo Gaidach <paavo.gaidach@gmail.com>
2021-09-27 14:53:53 +0300

    Made some changes to outgoing message and deleted unused stuff, also added comments

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Alertbot
  
-Alertbot that sends crypto price data to a discord server
+Alertbot that sends crypto price data to a discord server.
+Can be installed and used on a standalone server by installing requirements.txt and selecting the following;
+
+pair = 'LUNA/USDT'
+WEBHOOK_URL = "https://discord.com/api/webhooks/889265250875080725/nAh5DI_8uI6QDQYWW3GtQ4c1rUtK766mvDX9QTKyBseZiDOfMQR16iXGbPk--KGMY-Ts"
+DISCORD_USERID = "<@606915182459093032>"
--- alertbot.py
+++ alertbot.py
@@ -3,71 +3,58 @@ import pandas_ta as ta
 import pandas as pd
 import requests
 
+#asetetaan haluttu exchange, pari, aikaväli ja kuinka monta askelta taakse katsotaan.
 exchange = ccxt.binance()
-
-bars = exchange.fetch_ohlcv('LUNA/USDT', timeframe='1m', limit=40)
+pair = 'LUNA/USDT'
+bars = exchange.fetch_ohlcv(pair, timeframe='5m', limit=100)
 df = pd.DataFrame(bars, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
 
+#linkitetään halutut muuttujat
 adx = df.ta.adx()
 macd = df.ta.macd(fast=14, slow=28)
 rsi = df.ta.rsi()
 
-ema = ta.ema(df['close'],14)
-
-df = pd.concat([df, adx,macd,rsi,ema], axis=1)
-
+#Discordin webhook url sekä user ID jota halutaan pingata
 WEBHOOK_URL = "https://discord.com/api/webhooks/889265250875080725/nAh5DI_8uI6QDQYWW3GtQ4c1rUtK766mvDX9QTKyBseZiDOfMQR16iXGbPk--KGMY-Ts"
+DISCORD_USERID = "<@606915182459093032>"
 
-#print (df)
+#haetaan taulukkoon viimeiset 100 muuttujaa ja mitä arvoja niistä haetaan
+df = pd.concat([df, adx,macd,rsi], axis=1)
 
-
+#haetaan em taulukosta viimeinen rivi
 last_row = df.iloc[-1]
 
+#tulostetaan viimeinen rivi
 print(last_row)
 
 
-emavalue = f"ema arvo {last_row['EMA_14']:.2f}"
-
+#tehdään muuttujat ja otetaan tiedot niihin em. taulukon viimeisestä rivistä
 rsivalue = f"rsi arvo {last_row['RSI_14']:.2f}"
+adxvalue = f"adx arvo {last_row['ADX_14']:.2f}"
+macdLvalue = f"macdL arvo {last_row['MACD_14_28_9']:.2f}"
+macdHvalue = f"macdH arvo {last_row['MACDh_14_28_9']:.2f}"
 
-message = (rsivalue +"    " +emavalue)
 
+#nyt voidaan tehdä haluttu logiikka jonka perusteella lähetetään viesti
+#esimerkkitapauksessa pingataan käyttäjää jos macdLvalue > macdHvalue, muuten ei pingata
+if macdLvalue > macdHvalue:
+    message = ( DISCORD_USERID +"  " + pair + " on nousemassa  " + rsivalue +"  " + macdLvalue + "  " + macdHvalue + " https://www.tradingview.com/chart/?symbol=BINANCE%3ALUNAUSDT")
+
+else:   
+    message = ("Ei nousua " + pair + " " + rsivalue +"  " + macdLvalue + "  " + macdHvalue)
+
+#tulostetaan muuttujat lopuun
 print(rsivalue)
-print (emavalue)
+print(macdHvalue)
+print(macdLvalue)
+#tulostetaan viesti
 print("viesti" , message)
 
+#asetetaan viesti yhdeksi paketiksi
 payload = {
-"username": "alertbot", 
+"username": "Kaapo bot", 
 "content": message
 }
 
+#lähetetään viesti käyttäen aiemmin syötettyjä tietoja.
 requests.post(WEBHOOK_URL, payload)
-
-
-"""
-
-WEBHOOK_URL = "https://discord.com/api/webhooks/889249553142927430/MaXiE0SJXnu50xNyAVbOXqRIQGBX86S8lkU6lzV3S21CEyQuldb-CJZdTW2a2fX3bNMze"
-
-if last_row['ADX_14'] >= 25:
-    if last_row['DMP_14'] > last_row['DMN_14']:
-        message = f"STRONG UPTREND: The ADX is {last_row['ADX_14']:.2f}"
-    if last_row['DMN_14'] > last_row['DMP_14']:
-        message = f"STRONG DOWNTREND: The ADX is {last_row['ADX_14']:.2f}"
-
-    payload = {
-        "username": "Cryptobot",
-        "content": message
-    }
-
-    requests.post(WEBHOOK_URL, json=payload)
-
-if last_row['ADX_14'] < 25:
-    message = f"NO TREND: The ADX is {last_row['ADX_14']:.2f}"
-
-    payload = {
-        "username": "alertbot",
-        "content": message
-    }
-
-    requests.post(WEBHOOK_URL, json=payload)
-"""
